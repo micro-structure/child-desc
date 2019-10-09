af35946b94494205c95e2f38c1e71272384ff824
yangming <472102644@qq.com>
2019-10-09 22:14:40 +0800

    add todo

--- src/router.js
+++ src/router.js
@@ -1,7 +1,14 @@
+import Home from './views/Home.vue'
+
+// todo
+// 目前有个问题，即开发阶段，不能使用按需引入，因为待加载的模块 devServer 不同
+// 打包后的按需引入还必须要保证子项目之间 webpackChunkName: '' 不被重复冲突
+
 window.router.addRoutes([
   {
     path: '/',
     name: 'home',
-    component: () => import('./views/Home.vue')
+    // component: () => import('./views/Home.vue')
+    component: Home
   }
 ])
