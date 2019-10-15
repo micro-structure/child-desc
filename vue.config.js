7a86717a9dd0d2468aadda795f3d6200731453cb
yangming <472102644@qq.com>
2019-10-15 16:25:00 +0800

    fix: cmd

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,26 @@ module.exports = {
       'vue-router': 'VueRouter',
       'vuex': 'Vuex'
     }
+  },
+
+  chainWebpack: config => {
+    config
+      .optimization.splitChunks({
+      cacheGroups: {
+        vendors: {
+          name: `chunk-vendors`,
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          chunks: 'initial'
+        },
+        common: {
+          name: `chunk-common`,
+          minChunks: 2,
+          priority: -20,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        }
+      }
+    })
   }
 }
